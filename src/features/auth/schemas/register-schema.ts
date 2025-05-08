076e8ba4324ccc077374cc273a1e0e21def8db66
fadelafuente <f.delafuente@outlook.com>
2025-05-07 20:51:09 -0500

    fix spelling and change one message

--- src/features/auth/schemas/register-schema.ts
+++ src/features/auth/schemas/register-schema.ts
@@ -4,14 +4,14 @@ import { z } from "zod"
 
 export const registerSchema = z.object({
   username: z.string().min(4, {
-    message: "Usename must be at least 4 characters."
+    message: "Username must be at least 4 characters."
   }),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters."
   }),
   re_password: z.string()
 }).refine(value => value.password === value.re_password, {
-  message: "Passwords must match.",
+  message: "Passwords do not match.",
   path: ['re_password'],
 })
 
