bb640126f7219e1eab404d7e1873fadc4efb5e19
fadelafuente <f.delafuente@outlook.com>
2025-05-08 20:10:59 -0500

    add a return if auth isLoading since without it, the select-genres page gets redirect to the login page and then the index page on reload

--- src/routes/(auth)/_auth.tsx
+++ src/routes/(auth)/_auth.tsx
@@ -1,12 +1,21 @@
 import { useAuth } from "@/features/auth/hooks/use-auth";
 import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router";
+import { Loader2 } from "lucide-react";
 
 export const Route = createFileRoute("/(auth)/_auth")({
   component: Auth,
 });
 
 function Auth() {
-  const { data: user } = useAuth();
+  const { data: user, isLoading } = useAuth();
+
+  if(isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="size-8 animate-spin" />
+      </div>
+    );
+  }
 
   if (!user) {
     return <Navigate to={"/login"} />;
